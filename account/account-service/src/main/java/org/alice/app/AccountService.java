c2eb09a64d6829dcaf638f3b0f478cb0380d6780
alice <alicefeedback@163.com>
2014-04-06 21:14:41 +0800

    init

---
+++ account/account-service/src/main/java/org/alice/app/AccountService.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.alice.app;
+
+/**
+ * @author alice
+ *
+ */
+public class AccountService {
+
+	public void doSomething(){
+		System.out.println("AccountService.doSomething()");
+	}
+}
